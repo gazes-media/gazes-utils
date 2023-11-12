fca566af9bd56d8fb0d9586a821240091bdfd6e5
garder500 <jeremy27.clara22@gmail.com>
2023-11-12 21:45:06 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 current_dir=$(pwd)
 echo "Adding the load script to your .bashrc file"
-echo "$current_dir/load.sh" >> ~/.bashrc
+echo ".$current_dir/load.sh" >> ~/.bashrc
 echo "Done"
 echo "Executing source ~/.bashrc"
 source ~/.bashrc
