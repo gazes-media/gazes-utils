f7ff696531d92d54bbd7da7740ba6aa80ab20da7
garder500 <jeremy27.clara22@gmail.com>
2023-11-13 00:50:53 +0100

    Updating docker-clean

--- docker-utils/docker-run.sh
+++ docker-utils/docker-run.sh
@@ -1,13 +1,41 @@
  # Check if all arguments are provided
  docker-run() {
-    if [ "$#" -ne 3 ]; then
-        echo "Usage: docker-run <port> <container-name> <image>"
+    if [ "$#" -lt 3 ]; then
+        echo "Usage: docker-run <port> <container-name> <image> [ -e <env-var> -e <env-var> ... ]"
         return 1
     fi
 
     local port="$1"
     local container_name="$2"
     local image="$3"
+    # parse every -e argument
+    local envs=()
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -e)
+                # Check if there is a subsequent argument
+                if [[ $# -lt 2 ]]; then
+                    echo "Error: Missing argument for -e option."
+                    exit 1
+                fi
+
+                # Add the argument to the array
+                envs+=("$2")
+                shift 2
+                ;;
+            *)
+                # Ignore other options or arguments
+                shift
+                ;;
+        esac
+    done
+
+   local env_to_add=""
+    for env_var in "${envs[@]}"; do
+         env_to_add+=" -e $env_var"
+    done
+
 
     # Check if a container using this port already exists
     if docker ps --filter "publish=$port" --format '{{.Names}}' | grep -q "^$container_name$"; then
@@ -16,7 +44,7 @@
     fi
 
     # Run the Docker container
-    docker run -d -p "$port:$port" -e "PORT=$port" --name "$container_name" --restart always "$image"
+    docker run -d -p "$port:$port" -e "PORT=$port" --name "$container_name" $env_to_add --restart always "$image"
 }
 
 docker-run "$@"
