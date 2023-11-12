edfc5815114a60fbfc0a2fa94b47683da74edc85
garder500 <jeremy27.clara22@gmail.com>
2023-11-13 00:40:36 +0100

    Updating docker-clean

--- docker-utils/docker-clean.sh
+++ docker-utils/docker-clean.sh
@@ -22,6 +22,8 @@ docker-clean() {
     # Get environment variables before removing the container 
     local env_variables=$(docker exec "$container_name" env | cut -d= -f1)
 
+    local image_already_pulled=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep -q "^$new_image$")
+
     local env_to_add=""
     for env_var in $env_variables; do
         env_value=$(docker exec "$container_name" printenv "$env_var")
@@ -30,7 +32,8 @@ docker-clean() {
     local start_new_container="docker run -d --restart always --name $container_name $env_to_add"
 
     # Stop and remove the existing container
-    docker stop "$container_name" && docker rm "$container_name"
+    docker stop "$container_name" && docker rm "$container_name" > /dev/null
+    echo "The container $container_name has been stopped and removed."
 
     if [ -n "$old_image_id" ]; then
         if [ "$(diff <(echo "$old_image_id") <(echo "$new_image"))" != "" ]; then
@@ -38,9 +41,11 @@ docker-clean() {
             if docker ps -a --filter "ancestor=$old_image_id" --format '{{.Names}}' | grep -q "^$container_name$"; then
                 echo "The old image is still in use by other containers so it won't be removed."
             else
-                docker rmi "$old_image_id"
-                    # Pull the new image
-                docker pull "$new_image"
+                docker rmi "$old_image_id" > /dev/null
+            fi
+            
+            if [ -z "$image_already_pulled" ]; then
+                docker pull "$new_image" > /dev/null
             fi
         else
             echo "The old image is the same as the new image so it won't be removed."
@@ -57,7 +62,7 @@ docker-clean() {
     start_new_container="$start_new_container $new_image"
     # start the new container
     eval "$start_new_container"
-
+    echo "The new container $container_name has been started."
 
 }
 
