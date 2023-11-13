3477cf4dce74ea151aa3b4ea3f3a56cbf6d22e63
garder500 <jeremy27.clara22@gmail.com>
2023-11-13 01:00:13 +0100

    Updating docker-clean

--- docker-utils/docker-clean.sh
+++ docker-utils/docker-clean.sh
@@ -13,13 +13,15 @@ docker-clean() {
     local container_name="$1"
     local new_image="$2"
 
-    # Get the image ID before removing the container
-    local old_image_id=$(docker inspect -f '{{.Config.Image}}' "$container_name")
-    # if no old image id, then the container doesn't exist
-    if [ -z "$old_image_id" ]; then
-        echo "The container $container_name doesn't exist."
+    # Check if the container exists
+    if ! docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+        echo "Error: The container $container_name does not exist."
         return 1
     fi
+
+    # Get the image ID before removing the container
+    local old_image_id=$(docker inspect -f '{{.Config.Image}}' "$container_name")
+
 
     # Get the port mapping before removing the container
     local port_mapping=$(docker port "$container_name" | cut -d' ' -f3 | cut -d: -f2)
